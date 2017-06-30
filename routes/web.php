31e0ba839c797a9b6d0d70dac16008c31c886454
João D. Ferreira <jotomicron@gmail.com>
2017-06-30 02:37:31 +0100

    Start laying out the routes and controllers for the app

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,16 @@
  */
 Auth::routes();
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')->name('home');
+Route::get('/about', 'HomeController@about')->name('about');
+
+Route::get('/collect', 'Collector@show')->name('collect');
+Route::get('/random-pair', 'Collector@randomPair')->name('random-pair');
+Route::post('/collect', 'Collector@collect');
+
+Route::get('/profile', 'User@showProfile')->name('user.profile');
+Route::get('/profile/edit', 'User@showEditProfileForm')->name('user.profile.edit');
+Route::post('/profile', 'User@postProfile');
+Route::get('/profile/history', 'User@showHistory')->name('user.history');
+
+Route::get('/interests', 'Interests@get')->name('interests');
